9b7af48a4485c004721f8af632b61f6d4a6469ec
Ope Abidemi <opeabidemi@gmail.com>
2021-10-29 13:55:42 -0700

    added tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div className="bg-gray-900 h-screen w-screen text-white">
+    <Component {...pageProps} />
+  </div>
 }
 
 export default MyApp
