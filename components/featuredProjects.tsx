81cd23676d8c6b31ac359f4039f0c05e86a4a193
Ope Abidemi <opeabidemi@gmail.com>
2021-10-30 01:41:11 -0700

    design

---
+++ components/featuredProjects.tsx
@@ -0,0 +1,16 @@
+const FeaturedProjects = () => {
+    return (
+        <div className="py-10">
+            <p className="flex items-center space-x-4 px-6 font-bold tracking-wide text-gray-600">
+            <span className="text-xs">Featured Projects </span>
+            <span className="border-b w-36 h-1 border-gray-600"></span>
+            </p>
+            <div className="px-6 font-semibold text-gray-400 py-3">
+            <h3 className="text-3xl font-bold mb-10"><span className="text-primary text-opacity-70">{">_"}</span> ls projects 📁</h3>
+            <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default FeaturedProjects
