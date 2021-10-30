20c96d7a7ae340453bc8c81f06a05df599a0742c
Ope Abidemi <opeabidemi@gmail.com>
2021-10-30 11:47:24 -0700

    added resume page

--- components/aboutme.tsx
+++ components/aboutme.tsx
@@ -1,12 +1,12 @@
 const AboutMe = () => {
     return (
-        <div className="py-10">
+        <div className="py-10" id="about">
           <p className="flex items-center space-x-4 px-6 font-bold tracking-wide text-gray-600">
             <span className="text-xs">About Me </span>
             <span className="border-b w-36 h-1 border-gray-600"></span>
           </p>
           <div className="px-6 font-semibold text-gray-400 py-3">
-            <h3 className="text-3xl font-bold mb-10"><span className="text-primary text-opacity-70">{">_"}</span> whoami 😁</h3>
+            <h3 className="text-4xl font-bold mb-10"><span className="text-primary text-opacity-70">{">_"}</span> whoami 😁</h3>
             <p>
               Hello! My name is Abidemi and I love building high-quality apps and websites.
             </p>
---
+++ components/artdirection.tsx
@@ -0,0 +1,39 @@
+import { motion, useMotionValue, useViewportScroll, useDomEvent } from "framer-motion"
+import React from "react";
+
+const Artdirection = () => {
+    const { scrollYProgress } = useViewportScroll();
+    const art1 = useMotionValue(scrollYProgress.get());
+    const art2 = useMotionValue(scrollYProgress.get());
+
+    React.useEffect(() => {        
+        scrollYProgress.onChange((v) => {
+            art1.set(v * -2000)
+            art2.set(v * 2000)
+        });
+        
+    }, [ scrollYProgress ])
+    return (
+        <div className="py-4 text-8xl uppercase text-gray-600 font-bold flex flex-col space-y-6">
+            <span className="border-b w-full h-1 border-gray-600"></span>
+            <span className="text-xs text-primary">{">_"} Web Developer</span>
+            <motion.span
+                style={{
+                    width : "100%",
+                    translateX : art1
+                }}
+            className="w-screen whitespace-nowrap art text-gray-600 hover:text-gray-400 transition-all duration-300">
+                <motion.span className="ml-art1">I build quality, user-friendly websites</motion.span></motion.span>
+            <motion.span  
+                style={{
+                    width : "100%",
+                    translateX : art2
+                }}
+                className="w-screen whitespace-nowrap art text-gray-600 hover:text-gray-400 transition-all duration-300 ml-art2">I build cross-platform, scalable mobile apps.</motion.span>
+            <span className="text-xs self-end text-primary">{">_"} Mobile App Developer</span>
+            <span className="border-b w-full h-1 border-gray-600 "></span>
+        </div>
+    )
+}
+
+export default Artdirection
