142d73a5fa7e25c4c9306c30f7ee1f4a85a72727
Ope <opeabidemi@gmail.com>
2022-05-03 20:50:18 +0100

    new design

--- .prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    endOfLine: "lf",
-    semi: true,
-    singleQuote: false,
-    printWidth: 80,
-    tabWidth: 2,
-    jsxSingleQuote: true
-  }
--- utils/scroll.ts
+++
@@ -1,3 +0,0 @@
-export const scrollToView = (id : string) => {
-    document.getElementById(id)?.scrollIntoView({ behavior : "smooth" })
-}
