a09d378952e0a8d7a111d28ffa7eff23bb835bf4
Ope <opeabidemi@gmail.com>
2022-05-03 20:45:03 +0100

    changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  images : {
-    domains: ['media.giphy.com'],
-  }
-}
+  images: {
+    domains: ["media.giphy.com"],
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
         primary: "#FF2121",
-        secondary: "#050506"
-      }
+        secondary: "#050506",
+      },
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
