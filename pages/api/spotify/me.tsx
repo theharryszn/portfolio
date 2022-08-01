f18e6cb1e32bad0b6abb6f14ae144e8840b9415f
Abidemi Ope <opeabidemi@gmail.com>
2022-08-01 20:47:18 +0100

    changes

--- pages/api/spotify/me.tsx
+++ pages/api/spotify/me.tsx
@@ -1,4 +1,3 @@
-import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 import {
   getAccessToken,
--- pages/api/spotify/playlists.tsx
+++
@@ -1,5 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).send(process.env.SPOTIFY_CLIENT_ID);
-}
